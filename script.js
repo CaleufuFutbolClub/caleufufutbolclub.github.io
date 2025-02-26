693d8fc166af42e2569b0f5d2de2641037c2d931
CaleufuFutbolClub <caleufufutbolweb@gmail.com>
2025-02-26 18:32:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navList = document.getElementById('nav-list');
-    navList.classList.toggle('active');
-});
-document.getElementById('menu-icon').addEventListener('click', function() {
-    document.getElementById('nav-list').classList.toggle('active');
-});
 // Obtener el ícono del menú y la lista del menú
 const menuIcon = document.getElementById("menu-icon");
 const navList = document.getElementById("nav-list");
